4017b60259272a6a5b51387803c261375038c850
apizzimenti <pizzimentianthony@gmail.com>
2015-06-01 22:41:49 -0500

    cleanup

--- src/Reader.java
+++ src/Reader.java
@@ -3,31 +3,31 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Reader {
-	
+
 	private final String word;
 	private int points = 110;
-	
+
 	public Reader() {
 		word = read();
 	}
-	
+
 	public String getWord() {
 		return word;
 	}
-	
+
 	public void editPoints(int add) {
 		points -= add;
 	}
-	
+
 	public int getPoints() {
 		return points;
 	}
-	
+
 	public String read() {
-		String last = null;	
+		String last = null;
 		int finder = (int)(Math.random() * 50);
-		File newer = new File("../MasterMind Game/src/Words.txt");
-		
+		File newer = new File("../MasterMind/src/Words.txt");
+
 		try {
 			Scanner in = new Scanner(newer);
 			for (int i = 0; i < finder; i++){
@@ -40,10 +40,10 @@ public class Reader {
 		return last;
 	}
 
-	
+
 	public String analyze(String a) {
 		String best = "";
-		
+
 		// splits words into arrays, initializes count
 		String [] data = word.split("");
 		String [] guess = a.split("");
@@ -57,7 +57,7 @@ public class Reader {
 				points += 2;
 			}
 		}
-		
+
 		// checks differing-index similarities
 		for (int j = 0; j < data.length; j++) {
 			int sin = 0;
@@ -71,12 +71,12 @@ public class Reader {
 			else
 				count -= sin;
 		}
-		
+
 		for (int l = 0; l < count; l++) {
 			best += "O";
 			points += 1;
 		}
-		
+
 		return best;
 	}
 }
